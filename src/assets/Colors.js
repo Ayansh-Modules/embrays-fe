74287bd55b14200c44bd69a663dfa1af53ff1560
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-06-29 22:22:36 +0530

    Folder Structure

---
+++ src/assets/Colors.js
@@ -0,0 +1,7 @@
+const layoutColors = {
+    neon :  "#D2FF03",
+    pink : "#F72585",
+    yellow : "#FCD205",
+    blue : "#3A0CA3",
+    violet : "#8338BD",
+}
--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // css: {
-  //   postcss: {
-  //     plugins: [
-  //       require('tailwindcss'),
-  //       require('autoprefixer'),
-  //     ],
-  //   },
-  // },
 });
