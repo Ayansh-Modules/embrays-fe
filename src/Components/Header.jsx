5b6cb1350beb3dad3a4f5ec2d1795c7ab51e22d1
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-07-03 18:55:49 +0530

    responsiveness

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,51 @@
 import React from "react";
-import { appData } from "../Data/AppData";
 import { IoDownloadOutline } from "react-icons/io5";
+import { appData } from "../Data/AppData";
 function Header() {
-  const company = appData.companyName
+  const company = appData.companyName;
   return (
     <>
-     <div className="navbar  h-[13vh] w-full bg-Layoutblue flex-row flex items-center justify-evenly overflow-x-auto font-semibold">
-      <div className="logo flex-row mr-[10rem] items-center justify-center text-white text-4xl" >{company}</div>
-      <div className="navlink flex-row flex items-center justify-center">
-        <div className="wrapper bg-Layouttrans h-[8vh] w-[22.5vw] rounded-xl flex items-center justify-center">
-        <a title="About Us" className="about text-white hover:text-black  rounded-xl  hover:bg-Layoutneon p-[10px]">About Us</a>
-        <a title="Services" className="service  text-white hover:text-black  rounded-xl  hover:bg-Layoutneon p-[10px]">Services</a>
-        <a title="How it works?" className="howitworks text-white hover:text-black  rounded-xl  hover:bg-Layoutneon p-[10px] ">How it works? </a>
+      <div className="navbar  h-[13vh] w-100vw bg-Layoutblue flex-row flex items-center justify-center overflow-x-auto font-semibold">
+        <div className="w-[40vw] h-full  flex items-center justify-center text-3xl font-semibold text-white">
+          {company}
         </div>
-      <button title="Click To Download" type="button" className=" portfolio ml-[20px] h-[7vh] w-[8vw] focus:ring-2  focus:ring-white  text-black bg-Layoutneon hover:bg-white  rounded-xl p-[3px] "> 
-        <a href="" className="flex-row flex items-center justify-center mr-[10px]"><IoDownloadOutline style={{height:"3vh",width:"3vw"}}/> Portfolio</a> </button>
-    </div>
+        <div className="w-[60vw] h-full  flex">
+          <div className="w-[600px]  flex items-center justify-end ">
+            <div className="wrapper bg-Layouttrans h-[3.5rem] w-[25rem] rounded-xl flex items-center justify-center mr-[20px]">
+              <a
+                title="About Us"
+                className="about text-white hover:text-black  rounded-xl  hover:bg-Layoutneon h-[3.5rem] w-[7rem] flex items-center justify-center"
+              >
+                About Us
+              </a>
+              <a
+                title="Services"
+                className="service  text-white hover:text-black  rounded-xl  hover:bg-Layoutneon h-[3.5rem] w-[8rem] flex items-center justify-center"
+              >
+                Services
+              </a>
+              <a
+                title="How it works?"
+                className="howitworks text-white hover:text-black  rounded-xl  hover:bg-Layoutneon p-[10px] h-[3.5rem] w-[10rem] flex items-center justify-center"
+              >
+                How it works?{" "}
+              </a>
+            </div>
+          </div>
+          <div className="w-[190px]  flex items-center justify-start">
+            <button
+              type="button"
+              className="h-[3.5rem] w-[8.6rem] focus:ring-2 text-lg focus:ring-white  text-black bg-Layoutneon hover:bg-white  rounded-xl  flex items-center justify-center "
+            >
+              <IoDownloadOutline style={{ height: "25px", width: "25px" }} />{" "}
+              <span className=" "> Portfolio</span>
+            </button>
+          </div>
+        </div>
       </div>
     </>
-   
-
   );
 }
 
 export default Header;
+
