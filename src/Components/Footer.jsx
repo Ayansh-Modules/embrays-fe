6cae2f43d7d1750fd7bfd5def44f2ccdb47a1c8e
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-07-05 00:24:32 +0530

    footer fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,17 +7,17 @@ function Footer() {
   const instagram = appData.urls.instagram
 
   return (
-    <div className="footer pt-[10px] pb-[10px] w-full bg-Layoutblue flex-col flex items-center justify-center ">
+    <div className="footer pt-[25px] pb-[10px] w-full bg-Layoutblue flex-col flex items-center justify-center">
       <div className=" flex  justify-between w-full  max-md:flex-col">
         <div className="socialMedia flex-row flex items-center justify-start ">
           <div className="flex-row flex items-center border h-[40px] w-[40px]  hover:bg-indigo-600 rounded-full justify-center mr-[2vw] ml-[2vw]">
-            <TfiLinkedin href={linkedin} color="white" />
+           <a href={linkedin}  target="_blank"><TfiLinkedin  color="white" /></a> 
           </div>
           <div className="flex-row flex items-center border h-[40px] w-[40px]  hover:bg-indigo-600 rounded-full justify-center ">
-            <GrInstagram href={instagram} color="white" />
+           <a href={instagram}  target="_blank"><GrInstagram color="white" /></a> 
           </div>
         </div>
-        <div className="pageLink  flex-row flex items-center  justify-evenly   max-md:w-full max-md:p-[20px]">
+        <div className="pageLink  flex-row flex items-center  justify-evenly   max-md:w-full p-[20px]">
           <div className="flex  max-sm:flex-col max-md:flex-col  max-md:w-[50vw] ">
             <div  href="#aboutus" className="home text-white text-sm ]  hover:text-Layoutneon pr-[20px]">
               HOME
@@ -37,7 +37,7 @@ function Footer() {
           </div>
         </div>
       </div>
-      <div className="copyright  text-gray-400 ">
+      <div className="copyright  text-gray-400 pt-[20px] pb-[5px]">
         Copyright © 2024, All Right Reserved
       </div>
     </div>
