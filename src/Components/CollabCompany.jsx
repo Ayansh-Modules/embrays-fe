ae97fa7ef3eebfae4eb98452e942434b8cd5689a
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-07-24 00:54:45 +0530

    fix:Collab card issue

--- src/Components/CollabCompany.jsx
+++ src/Components/CollabCompany.jsx
@@ -7,7 +7,7 @@ function CollabCompany(props) {
 
   return (
     <>
-    <div className=" collabcard  h-[25rem] min-w-[400px] w-[35vw] overflow-hidden bg-white rounded-2xl max-md:ml-[20vw]">
+    <div className="hidden md:block md:overflow-hidden  collabcard h-[25rem] min-w-[400px] w-[35vw] bg-white rounded-2xl max-md:ml-[20vw]">
       <img alt="collabCardBg"
         className="rounded-2xl min-w-[400px] w-[35vw] h-[25rem] object-cover"
         src={background}
@@ -23,15 +23,15 @@ function CollabCompany(props) {
     </div>
 
     <div className=" secondcard block md:hidden">
-      <div className="collabcard  w-[80vw] bg-white rounded-t-2xl">
+      <div className="collabcard  w-[80vw]  bg-white rounded-t-2xl">
         <img className="rounded-2xl" src={background} alt='collabCardBg' />
       </div>
       <div className="Mobile bg-Layoutblue w-[80vw] h-[16rem] flex items-center justify-center rounded-b-2xl">
         <div className="text w-[80%] text-white">
-          <p className="font-semibold text-2xl text-center pb-[10px]">
+          <p className="font-semibold text-2xl text-center pb-[10px] ">
             {company}
           </p>
-          <p className="text-xs">{testimonial}</p>
+          <p className="text-xs pb-5">{testimonial}</p>
         </div>
       </div>
      
